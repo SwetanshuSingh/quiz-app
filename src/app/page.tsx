947c12cbcf9e411e3b190a4ca4fd40ddb657d9e0
swetanshu <swetanshusingh17@gmail.com>
2024-09-07 11:25:29 +0530

    [feat]: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main>
-      <h2 className="">Hello World</h2>
+    <main className="w-full min-h-screen p-5 bg-[#0F0F10] text-[#E7E7E4]">
+      <Navbar />
     </main>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Puzzle } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex justify-between items-center">
+      <Puzzle className="w-6 h-6" />
+      <div className="flex justify-center items-center gap-8 text-lg">
+        <p className="cursor-pointer hover:underline hover:underline-offset-4">
+          Explore
+        </p>
+        <p>Leaderboard</p>
+        <p>Testimonials</p>
+        <p>Contact</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
