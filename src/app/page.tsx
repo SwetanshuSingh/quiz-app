095ad3945749afd99bb787c21284ac55cf51f561
swetanshu <swetanshusingh17@gmail.com>
2024-09-10 09:28:55 +0530

    [feat]: add community section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Community from "@/components/Community";
 import FeaturesSection from "@/components/Features";
 import HeroSection from "@/components/Hero";
 import Navbar from "@/components/Navbar";
@@ -10,7 +11,7 @@ export default function Home() {
         <HeroSection />
       </section>
       <FeaturesSection />
-      <section>helooo</section>
+      <Community />
     </main>
   );
 }
