c116b4063a84200832ca7832c5d6bf48260a5b96
swetanshu <swetanshusingh17@gmail.com>
2024-09-11 22:28:49 +0530

    [update]: add join dialog view styles

--- src/components/dialog-views/CreateDialogView.tsx
+++ src/components/dialog-views/CreateDialogView.tsx
@@ -9,7 +9,10 @@ type CreateDialogViewProps = {
 const CreateDialogView = ({ setDialogView }: CreateDialogViewProps) => {
   return (
     <div>
-      <button className="w-fit bg-[#E7E7E4] text-[#0F0F10] inline-flex h-12 items-center justify-center rounded-md px-8 font-medium shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+      <button
+        onClick={() => setDialogView("HOME")}
+        className="w-fit bg-[#E7E7E4] text-[#0F0F10] inline-flex h-10 items-center justify-center rounded-md px-6 font-semibold shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+      >
         Back
       </button>
     </div>
